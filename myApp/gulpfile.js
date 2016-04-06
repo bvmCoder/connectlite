7a1c0947ee95feb570a7a5961aa88cc264d8b2dd
BVM <talkwithdixit@gmail.com>
2016-04-06 17:17:00 -0400

    Update gulpfile.js

--- myApp/gulpfile.js
+++ myApp/gulpfile.js
@@ -1,33 +1,68 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    bower = require('bower'),
+    concat = require('gulp-concat'),
+    sass = require('gulp-sass'),
+    minifyCss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    sh = require('shelljs'),
+	uglify=require('gulp-uglify');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: [{src:'scss/**/*.scss',dest:'www/css/'}],  
+  js: [{src:'www/js/**/*.js',dest:'www/dist/'}]
 };
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass','minify','watchJs']);
+
+/*gulp.task('sass', function(done) {
+	  gulp.src('./scss/*.scss')
+	    .pipe(sass({
+	      errLogToConsole: true
+	    }))
+	    .pipe(gulp.dest('./www/css/'))
+	    .pipe(minifyCss({
+	      keepSpecialComments: 0
+	    }))
+	    .pipe(rename({ extname: '.min.css' }))
+	    .pipe(gulp.dest('./www/css/'))
+	    .on('end', done);
+	});
+gulp.task('watch', function() {
+	  gulp.watch(paths.sass, ['sass']);
+	});*/
+
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
     .pipe(sass())
     .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(gulp.dest(paths.sass[0].dest))
     .pipe(minifyCss({
       keepSpecialComments: 0
     }))
     .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(gulp.dest(paths.sass[0].dest))
     .on('end', done);
 });
 
+
+gulp.task('minify',function(){
+return gulp.src(paths.js[0].src)
+ .pipe(concat('app.js'))
+ .pipe(gulp.dest(paths.js[0].dest))
+ .pipe(rename({ extname: '.min.js' }))
+ //.pipe(uglify())
+ .pipe(gulp.dest(paths.js[0].dest));
+ });
+ // Watch Our Files
+gulp.task('watchJs',function(){
+ gulp.watch(paths.js[0].src,['minify']);
+ }); 
+
+
 gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.sass[0].src, ['sass']);
 });
 
 gulp.task('install', ['git-check'], function() {
