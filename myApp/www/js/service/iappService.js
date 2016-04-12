5c9a49fad1deafdceaca1173b96995bbd1a146d6
BVM <talkwithdixit@gmail.com>
2016-04-12 17:50:02 -0400

    Update iappService.js

--- myApp/www/js/service/iappService.js
+++ myApp/www/js/service/iappService.js
@@ -1,7 +1,7 @@
 angular.module('starter')
 .service('userInformationService', ['$http',function userInformationService($http) {
 	var _getCustomer = function _getCustomer(user) {
-		var promise=$http.get('http://10.149.65.144:8080/customerSearch',{params:user}).
+		var promise=$http.get('http://10.149.66.6:8080/customerSearch',{params:user}).
 			then(function(response){
 				return response.data;
 			},function(reason){
@@ -10,8 +10,19 @@ angular.module('starter')
 	
 		return promise;
 	};
+	var _getCustomerProfile = function _getCustomerProfile(ecid) {
+		var promise=$http.get('http://10.149.66.6:8080/customerProfile',{params:ecid}).
+			then(function(response){
+				return response.data;
+			},function(reason){
+				return "failure";
+			});
+	
+		return promise;
+	};	
 
 	return {
-		getCustomer : _getCustomer
+		getCustomer : _getCustomer,
+		getCustomerProfile : _getCustomerProfile
 	};
 }]);
